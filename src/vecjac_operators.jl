82a3ea6d37898ba4445b886860ed42b321d68260
Qiyao Wei <juventia2014@gmail.com>
2021-11-24 15:29:03 +0800

    Update vecjac_operators.jl

--- src/vecjac_operators.jl
+++ src/vecjac_operators.jl
@@ -103,7 +103,7 @@ function Base.:*(L::VecJacOperator, x::AbstractVector)
     end
     #println(typeof(L))
     #println(typeof(L.f))
-    temp = _u -> L.f(_u)
+    temp = _u -> L.f(_u, L.p, L.t)
     println(temp(x))
     println("end")
     return mul!(similar(vec(L.u)), L, x)
